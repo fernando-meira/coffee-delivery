00420378c026a040f7978990d7f0f7c2d3104237
Fernando Meira <fernando.meira@goomer.com.br>
2022-10-30 12:47:36 -0300

    feat: first commit

---
+++ src/styles/default.ts
@@ -0,0 +1,65 @@
+export const defaultTheme = {
+  border: {
+    radius: '0.4rem',
+  },
+  font: {
+    base: {
+      family:
+        "Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      regular: 'regular',
+      bold: 'bold',
+      lineHeight: {
+        '130': '130%',
+        '160': '160%',
+      },
+      sizes: {
+        10: '1rem',
+        12: '1.2rem',
+        14: '1.4rem',
+        16: '1.6',
+        18: '1.8rem',
+        20: '2.0rem',
+        24: '2.4rem',
+      },
+    },
+    header: {
+      family:
+        "Baloo+2, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      bold: '700',
+      extraBold: '800',
+      lineHeight: {
+        130: '130%',
+      },
+      sizes: {
+        18: '1.8rem',
+        20: '2.0rem',
+        32: '3.2rem',
+        48: '4.8rem',
+      },
+    },
+  },
+  colors: {
+    base: {
+      title: '#272221',
+      subtitle: '#403937',
+      text: '#574F4D',
+      label: '#8D8686',
+      hover: '#D7D5D5',
+      button: '#E6E5E5',
+      input: '#EDEDED',
+      card: '#F3F2F2',
+      background: '#FAFAFA',
+      white: '#FFFFFF',
+    },
+    yellow: {
+      '100': '#F1E9C9',
+      '500': '#DBAC2C',
+      '900': '#C47F17',
+    },
+    purple: {
+      '100': '#EBE5F9',
+      '500': '#8047F8',
+      '900': '#4B2995',
+    },
+  },
+};
---
+++ src/styles/global.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+export default createGlobalStyle`
+  ${({ theme }) => css`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      -moz-osx-font-smoothing: grayscale;
+      -webkit-font-smoothing: antialiased;
+    }
+
+    :focus {
+      outline: 0;
+      box-shadow: 0 0 0 2px ${theme.colors.purple[500]};
+    }
+
+    html {
+      font-size: 62.5%;
+    }
+
+    body,
+    input,
+    textarea,
+    button {
+      font-weight: 400;
+      font-family: 'Roboto', sans-serif;
+      font-size: ${theme.font.base.regular};
+    }
+
+    body {
+      color: ${theme.colors.base.text};
+      background-color: ${theme.colors.base.background};
+    }
+
+    button {
+      cursor: pointer;
+    }
+  `}
+`;
