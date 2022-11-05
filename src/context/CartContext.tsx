365448c0908e9285da198b20ed5086d46e1b5d88
Fernando Meira <fernando.meira@goomer.com.br>
2022-11-04 22:06:00 -0300

    feat: Iniciado o contexto do carrinho.

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState } from 'react';
+
+interface ItemsProps {
+  id: 0;
+  image: string;
+  title: string;
+  price: number;
+  amount: number;
+}
+
+interface CartContextType {
+  items?: ItemsProps[];
+}
+
+interface CartProviderProps {
+  children: React.ReactNode;
+}
+
+export const CartContext = createContext({} as CartContextType);
+
+function CartContextProvider({ children }: CartProviderProps) {
+  const [cart, setCart] = useState([]);
+
+  return (
+    <CartContext.Provider value={{ items: cart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+function useCart(): CartContextType {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error('useCart must be used within an CartProvider');
+  }
+
+  return context;
+}
+
+export { useCart, CartContextProvider };
